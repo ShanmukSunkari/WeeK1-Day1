e88a38ea33953dd1f3ea721c40295210be188109
ShanmukSunkari <42819365+ShanmukSunkari@users.noreply.github.com>
2026-02-28 17:55:41 +0530

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,18 @@
+
+var firstName = "Shanmuk"
+var companyName = "Oracle"
+var mobileNumber = "1234567899"
+var isAutomation = false
+var hasPlaywright 
+
+
+console.log(typeof firstName )
+console.log(typeof companyName )
+console.log(typeof mobileNumber )
+console.log(typeof isAutomation )
+console.log(typeof hasPlaywright )
+
+console.log("First name is : "+firstName)
+console.log("Company Name name is : "+companyName)
+console.log("Mobile number is : "+mobileNumber)
+console.log("Automation status : "+isAutomation)
