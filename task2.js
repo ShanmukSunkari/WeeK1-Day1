bdf7485a7acb578201ecda11efdac9eb915b5fd7
ShanmukSunkari <42819365+ShanmukSunkari@users.noreply.github.com>
2026-03-01 10:35:21 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,16 @@
+let userName = "shanmuk"
+let organization = "Google"
+let contactNumber = "9876543212"
+let knowsAutomation = true;
+let usesPlaywright
+
+console.log(typeof userName);
+console.log(typeof organization);
+console.log(typeof contactNumber);
+console.log(typeof knowsAutomation);
+console.log(usesPlaywright);
+
+console.log("User name is : " +userName);
+console.log("Organization is : " +organization );
+console.log("Automation :" +knowsAutomation);
+console.log("Contact number is :" +contactNumber);
